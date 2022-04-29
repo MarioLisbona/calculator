832db0c2f6d3967432c8a2b336418249d9f73b8d
Mario Lisbona <mario.lisbona@gmail.com>
2022-04-29 12:33:11 +1000

    Add eventlisenters for button animations

--- script.js
+++ script.js
@@ -33,9 +33,39 @@ function operate(operator, num1, num2){
 }
 
 
+//event listeners and functions for button animations
 const buttons = Array.from(document.querySelectorAll('.button'));
-buttons.forEach(button => button.addEventListener('mouseover', buttonHover));
 
-function buttonHover() {
-	console.log('Hovering over a button');
+buttons.forEach(button => button.addEventListener('mouseover', btnHover));
+buttons.forEach(button => button.addEventListener('mouseout', btnDefault));
+buttons.forEach(button => button.addEventListener('mousedown', btnClickDown));
+buttons.forEach(button => button.addEventListener('mouseup', btnClickUp));
+
+function btnHover(e) {
+	this.style.backgroundColor = '#BCBCBD';
+}
+
+function btnDefault(e) {
+	this.style.backgroundColor = '';
+}
+
+function btnClickDown(e) {
+	this.style.transform = 'scale(.95)';
+}
+
+function btnClickUp(e) {
+	this.style.transform = '';
+}
+
+
+//test function for outputing to display and clearing display
+
+const displayOp = document.querySelector('.display-operation');
+const btnClear = document.querySelector('#btnClear');
+
+buttons.forEach(button => button.addEventListener('click', testFunction));
+
+
+function testFunction(e) {
+	displayOp.textContent += e.target.textContent;	
 }
